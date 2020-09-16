67b76219b5f43b98725d02dec0f71abee8fab00b
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-09-16 23:07:57 +0530

    Refactor

--- drop.js
+++ drop.js
@@ -2,18 +2,19 @@ const emote = document.querySelector(".emote");
 const randomStartinLocation = Math.random() * window.innerWidth;
 emote.style.left = `${randomStartinLocation}px`;
 
-const startSpeed = 400;
-const mu = 0.03; // Friction amount (between 0 and 1)
+const startSpeed = 400 + Math.floor(100 * Math.random());
+const mu = 0.02; // Friction amount (between 0 and 1)
 
-let xspeed = startSpeed;
-let yspeed = startSpeed;
+let speed = startSpeed;
+let xdir = 1;
+let ydir = 1;
 
 const dropLoop = () => {
   let left = parseInt(emote.style.left) || 0;
   let top = parseInt(emote.style.top) || 0;
 
-  emote.style.left = `${left + xspeed}px`;
-  emote.style.top = `${top + yspeed}px`;
+  emote.style.left = `${left + speed * xdir}px`;
+  emote.style.top = `${top + speed * ydir}px`;
 
   checkCollision();
   applyFriction();
@@ -25,33 +26,24 @@ const checkCollision = () => {
   let left = parseInt(emote.style.left) || 0;
   let top = parseInt(emote.style.top) || 0;
 
-  if (xspeed > 0 && left + emote.clientWidth >= window.innerWidth) {
-    xspeed = -xspeed;
+  if (xdir > 0 && left + emote.clientWidth >= window.innerWidth) {
+    xdir *= -1;
   }
-  if (xspeed < 0 && left <= 0) {
-    xspeed = -xspeed;
+  if (xdir < 0 && left <= 0) {
+    xdir *= -1;
   }
-  if (yspeed > 0 && top + emote.clientHeight >= window.innerHeight) {
-    yspeed = -yspeed;
+  if (ydir > 0 && top + emote.clientHeight >= window.innerHeight) {
+    ydir *= -1;
   }
-  if (yspeed < 0 && top < 0) {
-    yspeed = -yspeed;
+  if (ydir < 0 && top < 0) {
+    ydir *= -1;
   }
 };
 
 const applyFriction = () => {
-  if (xspeed !== 0) {
-    if (Math.abs(xspeed) <= 10)
-      xspeed = Math.floor(Math.abs(xspeed) - 1) * (xspeed / Math.abs(xspeed));
-    else xspeed = Math.floor(xspeed * (1 - mu));
-  }
-  if (yspeed !== 0) {
-    if (Math.abs(yspeed) <= 10)
-      yspeed = Math.floor(Math.abs(yspeed) - 1) * (yspeed / Math.abs(yspeed));
-    else yspeed = Math.floor(yspeed * (1 - mu));
-  }
-
-  // console.log(xspeed, yspeed);
+  if (Math.abs(speed) <= 50)
+    speed = Math.floor(Math.abs(speed) - 1) * (speed / Math.abs(speed));
+  else speed = Math.floor(speed * (1 - mu));
 };
 
 dropLoop();
